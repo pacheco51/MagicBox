9263110613dcdb4b999d8500ce44d6bf13db91c5
botrockets <botrockets@gmail.com>
2017-09-21 13:34:22 +0530

    .

---
+++ MagicBoxLib/SearchDomains.fs
@@ -0,0 +1,25 @@
+﻿[<AutoOpen>]
+module SearchDomains
+
+type SearchBaseUrl = string
+type Keyword = string
+type QueryParam = string
+type Query = QueryParam * Keyword
+type NextPageParamName = string 
+type NextPageValue = int -> int 
+type PageNum = int 
+type AllOtherQueryParams = Map<string,string>
+type CssSelectors = ResultCss of string | NextPageCss of string
+
+type [<Measure>] DelayInMs
+
+type HtmlSearchEngine =
+  SearchBaseUrl * 
+  Query * 
+  NextPageParamName *
+  NextPageValue *
+  PageNum *
+  AllOtherQueryParams * 
+  CssSelectors *
+  int<DelayInMs> *
+  SearchProxy option
