9a700cd5873618b5782115ea27f00a8e2fc26b36
botrockets <botrockets@gmail.com>
2017-09-24 14:56:26 +0530

    Yahoo and Bing Parsers

--- MagicBoxLib/SearchEngines.fs
+++ MagicBoxLib/SearchEngines.fs
@@ -1,6 +1,7 @@
 ﻿module Google
 
 open FSharp.Data
+open System.Net
 open FSharp.Data.HttpResponseHeaders
 
 type SearchResult =
@@ -30,8 +31,7 @@ let Google =
       OtherQueryParams = None
       Parser = (fun html->let nodes = HtmlDocument.Parse(html).CssSelect("div.g") |> List.filter(fun n-> n.CssSelect("h3.r")|>List.isEmpty|>not)
                           if(nodes.IsEmpty) then
-                            //ToDo 
-                            //Check wheather Google blocked us
+                            //ToDo :: Check wheather Google blocked us
                             Error "Couldn't found results..."
                           else
                             Ok(nodes|>List.mapi(fun i n-> let a = List.head (n.CssSelect("h3 > a"))
@@ -52,3 +52,64 @@ let Google =
       HttpRequestData = DefaultRequestData
      }
 
+let Bing = {
+      SearchEngineName = "Bing"  
+      SearchBaseUrl = "http://www.bing.com/search"  
+      QueryParamName = Some "q"
+      NextPageUrl = fun html -> let nodes = HtmlDocument.Parse(html).CssSelect("a.sb_pagN")
+                                if List.length nodes >= 0 then (nodes |>List.head).AttributeValue("href")|>Some else None
+      OtherQueryParams = None
+      Parser = (fun html->let nodes = HtmlDocument.Parse(html).CssSelect("li.b_algo") 
+                          if(nodes.IsEmpty) then
+                            //ToDo :: Check wheather Bing blocked us
+                            Error "Couldn't found results..."
+                          else
+                            Ok(nodes|>List.mapi(fun i n-> let a = List.head (n.CssSelect("h2 > a"))
+                                                          let href = a.AttributeValue("href")
+                                                          let position = i+1
+                                                          let linkText = a.InnerText()
+                                                          let span = List.head (n.CssSelect("div > p"))
+                                                          let snippet = span.InnerText()
+                                                          {
+                                                            Url = href
+                                                            LinkText = linkText
+                                                            ResultPosition = position
+                                                            Snippet=snippet|>Some
+                                                          }
+                                                )))
+      HttpRequestData = DefaultRequestData
+     }
+     
+let Yahoo = {
+      SearchEngineName = "Yahoo"  
+      SearchBaseUrl = "https://search.yahoo.com/search"  
+      QueryParamName = Some "p"
+      NextPageUrl = fun html -> let nodes = HtmlDocument.Parse(html).CssSelect("a.next")
+                                if List.length nodes >= 0 then (nodes |>List.head).AttributeValue("href")|>Some else None
+      OtherQueryParams = None
+      Parser = (fun html->let nodes = HtmlDocument.Parse(html).CssSelect("div.dd.algo") 
+                          if(nodes.IsEmpty) then
+                            //ToDo :: Check wheather Bing blocked us
+                            Error "Couldn't found results..."
+                          else
+                            Ok(nodes|>List.mapi(fun i n-> let a = List.head (n.CssSelect("a.ac-algo"))
+                                                          let href = a.AttributeValue("href")
+                                                          let link=
+                                                            if(href.StartsWith("http")) then href
+                                                            else
+                                                                let index = href.IndexOf("http")
+                                                                let lastIndex = href.IndexOf("/")
+                                                                href.[index..lastIndex]
+                                                          let position = i+1
+                                                          let linkText = a.InnerText()
+                                                          let p = List.head (n.CssSelect("div.compText.aAbs p"))
+                                                          let snippet = p.InnerText()
+                                                          {
+                                                            Url = link
+                                                            LinkText = linkText
+                                                            ResultPosition = position
+                                                            Snippet=snippet|>Some
+                                                          }
+                                                )))
+      HttpRequestData = DefaultRequestData
+     }
